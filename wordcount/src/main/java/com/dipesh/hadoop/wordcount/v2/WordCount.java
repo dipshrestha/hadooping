8e610fbb787e6a7b60926efcf5155c420da34c21
dipesh <dipshrestha@gmail.com>
2016-06-11 17:12:52 -0700

    Adding configurations

--- wordcount/src/main/java/com/dipesh/hadoop/wordcount/v2/WordCount.java
+++ wordcount/src/main/java/com/dipesh/hadoop/wordcount/v2/WordCount.java
@@ -1,6 +1,8 @@
 package com.dipesh.hadoop.wordcount.v2;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -13,6 +15,15 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * @author dshrestha
+ * 
+ * A Tool interface that supports handling of generic command-line options.
+ * Tool, is the standard for any Map-Reduce tool/application.
+ * 
+ *
+ */
 public class WordCount extends Configured implements Tool{
 	private final static Logger logger = Logger.getLogger(WordCount.class);
 	
@@ -29,12 +40,18 @@ public class WordCount extends Configured implements Tool{
 			ToolRunner.printGenericCommandUsage(System.err);
 			return -1;
 		}
-	
-		// job configurations
-		Job job = new org.apache.hadoop.mapreduce.Job();
-		job.setJarByClass(WordCount.class);
-		job.setJobName("WordCounter");
-	
+		
+		// input/output dir from command-line
+		Path inputPath = new Path(args[0]);
+		Path outputPath = new Path(args[1]);
+	        
+		// create a new configuration
+        Configuration conf = new Configuration(true);
+        
+        // job configurations
+        Job job = new Job(conf, getClass().getCanonicalName());
+		job.setJarByClass(getClass());
+		
 		// executor configurations
 		job.setMapperClass(WordCountMapper.class);
 		job.setCombinerClass(WordCountReducer.class);
@@ -47,10 +64,17 @@ public class WordCount extends Configured implements Tool{
 		job.setInputFormatClass(TextInputFormat.class);
 		
 		// input/output file configurations
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-		int returnValue = job.waitForCompletion(true) ? 0:1;
+		FileInputFormat.addInputPath(job, inputPath);
+		FileOutputFormat.setOutputPath(job, outputPath);
+		
+        // delete output if exists
+		FileSystem fs = FileSystem.get(conf);
+		if(fs.exists(outputPath)) {
+			logger.info("Deleting directory..." + outputPath.getName());
+			fs.delete(outputPath, true);			
+		}
+		
+		int returnValue = job.waitForCompletion(true) ? 0 : 1;
 		logger.info("job.isSuccessful " + job.isSuccessful());
 		return returnValue;
 	}
